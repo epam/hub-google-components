972d839c943b21fe9866e2622e9758d8899c0a0d
Viktors Oginskis <viktors_oginskis@epam.com>
2022-03-16 16:26:29 +0200

    Component updates to support Anthosless gke app

--- online-boutique-app/undeploy.sh
+++ online-boutique-app/undeploy.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 gcloud container clusters get-credentials "$NAME" --zone "$ZONE"
-kubectl config delete-context "$DOMAIN_NAME" > /dev/null 2>&1 
+kubectl config delete-context "$DOMAIN_NAME" >/dev/null 2>&1
 kubectl config rename-context gke_"$PROJECT"_"$ZONE"_"$NAME" "$DOMAIN_NAME"
 
-kubectl delete --context="$DOMAIN_NAME" -f resources/kubernetes-manifests/deployments
-kubectl delete --context="$DOMAIN_NAME" -f resources/kubernetes-manifests/services
-kubectl delete --context="$DOMAIN_NAME" -f resources/istio-manifests/allow-egress-googleapis.yaml
-kubectl delete --context="$DOMAIN_NAME" -f resources/istio-manifests/frontend-gateway.yaml
+kubectl="kubectl --context=$DOMAIN_NAME"
 
-kubectl delete --context="$DOMAIN_NAME" -f resources/kubernetes-manifests/namespaces
+$kubectl delete -f resources/kubernetes-manifests/deployments
+$kubectl delete -f resources/kubernetes-manifests/services
+if test -z "$ISTIO_REVISION"; then
+	$kubectl delete -f resources/frontend-ingress.yaml
+else
+	$kubectl delete -f resources/istio-manifests/allow-egress-googleapis.yaml
+	$kubectl delete -f resources/istio-manifests/frontend-gateway.yaml
+fi
+
+$kubectl delete -f resources/kubernetes-manifests/namespaces
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
-    echo "Resources do not exist in Kubernetes. Already undeployed?"
-    exit 0
+	echo "Resources do not exist in Kubernetes. Already undeployed?"
+	exit 0
 fi
