162dbf2832696fa4cf61c599b0c0e38c8ca25cd0
Viktors Oginskis <viktors_oginskis@epam.com>
2022-03-21 14:21:24 +0200

    typo

--- k6-sql-load-test/deploy.sh
+++ k6-sql-load-test/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xk6 build master --with github.com/imiric/xk6-sql
+xk6 build master --with github.com/grafana/xk6-sql
 ./k6 run "$TEST_NAME-$KIND.js" --duration "$DURATION" --vus "$USERS"
