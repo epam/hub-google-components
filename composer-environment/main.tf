8919b15401841c0cc47ca5e7a8f7bec792ae11d4
Antons Kranga <akranga@gmail.com>
2022-03-23 00:24:54 +0100

    wiring network component

--- composer-environment/main.tf
+++ composer-environment/main.tf
@@ -7,6 +7,14 @@ provider "google" {}
 
 data "google_client_config" "current" {}
 
+data "google_compute_subnetwork" "this" {
+  self_link = var.subnetwork
+}
+
+locals {
+  network_name = basename(data.google_compute_subnetwork.this.network)
+}
+
 module "composer" {
   source  = "terraform-google-modules/composer/google"
   version = "~> 2.0"
@@ -15,6 +23,6 @@ module "composer" {
   project_id        = data.google_client_config.current.project
   region            = data.google_client_config.current.region
   zone              = data.google_client_config.current.zone
-  network           = var.common_name
-  subnetwork        = var.common_name
+  network           = local.network_name
+  subnetwork        = data.google_compute_subnetwork.this.name
 }
--- composer-environment/variables.tf
+++ composer-environment/variables.tf
@@ -2,7 +2,8 @@ variable "common_name" {}
 
 variable "node_count" {
   type    = number
-  default = 3
 }
 
 variable "machine_type" {}
+
+variable "subnetwork" {}
