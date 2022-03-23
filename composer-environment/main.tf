6ec4a3ed920b700a5ce88d52be18068dda3b485b
Antons Kranga <akranga@gmail.com>
2022-03-23 13:50:27 +0100

    Moved to compose create module

--- composer-environment/main.tf
+++ composer-environment/main.tf
@@ -12,17 +12,19 @@ data "google_compute_subnetwork" "this" {
 }
 
 locals {
-  network_name = basename(data.google_compute_subnetwork.this.network)
+  network_name  = basename(data.google_compute_subnetwork.this.network)
+  image_version = var.composer_version == "v2" ? "composer-2.0.0-preview.3-airflow-2.1.2" : null
 }
 
 module "composer" {
-  source  = "terraform-google-modules/composer/google"
-  version = "~> 2.0"
-
-  composer_env_name = var.common_name
+  source            = "terraform-google-modules/composer/google//modules/create_environment"
+  composer_env_name = var.composer_env_name
   project_id        = data.google_client_config.current.project
   region            = data.google_client_config.current.region
+  image_version     = local.image_version
+  node_count        = var.node_count
   zone              = data.google_client_config.current.zone
+  machine_type      = var.node_machine_type
   network           = local.network_name
   subnetwork        = data.google_compute_subnetwork.this.name
 }
--- composer-environment/variables.tf
+++ composer-environment/variables.tf
@@ -1,9 +1,13 @@
-variable "common_name" {}
+variable "composer_env_name" {}
 
 variable "node_count" {
-  type    = number
+  type = number
 }
 
-variable "machine_type" {}
+variable "node_machine_type" {}
 
 variable "subnetwork" {}
+
+variable "composer_version" {
+  default = "v1"
+}
