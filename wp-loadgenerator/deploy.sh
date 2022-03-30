0df950edbf52967c4fc3e3ef2fa5a214ef7f4f1b
Antons Kranga <akranga@gmail.com>
2022-03-30 14:31:58 +0200

    Fine tuning locust experience

--- wp-loadgenerator/deploy.sh
+++ wp-loadgenerator/deploy.sh
@@ -7,11 +7,18 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip3 install --user "locust==2.8.4"
 
+expected="200"
 success=""
+cat << EOF
+Waiting when $URL becomes available (expected http code: $expected)
+in the progress printing first letter of http response
+where: 0: not available, 2: OK, 4: HTTP error, 5: service error
+
+EOF
+
 echo "Waiting when $URL becomes available"
 echo "Printing first letter of http response"
 echo -n "Where: 0: not available, 2: OK, 4: HTTP error, 5: service error: " 
-expected="200"
 # 120 * 5 = 600sec
 for _ in $(seq 120); do
     # shellcheck disable=SC1083
@@ -26,7 +33,7 @@ for _ in $(seq 120); do
 done
 echo
 if test -z "$success"; then
-    color w "Warning: timed out to wait "
+    color w "Timed out!"
 fi
 
 locust --config locust.conf
