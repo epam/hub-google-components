df55eaa244b978b5a6d9802e5c25ff087835c524
Antons Kranga <akranga@gmail.com>
2022-03-30 11:20:28 +0200

    Fine tuning constraints

--- composer-environment/main.tf
+++ composer-environment/main.tf
@@ -20,7 +20,7 @@ locals {
 # Follow publishing process can here: https://github.com/terraform-google-modules/terraform-google-composer/pull/40 
 module "composer" {
   source            = "terraform-google-modules/composer/google//modules/create_environment"
-  version           = "~> 2.4.0"
+  version           = ">= 2.4.0"
   composer_env_name = var.composer_env_name
   project_id        = data.google_client_config.current.project
   region            = data.google_client_config.current.region
