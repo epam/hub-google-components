12f69cd3b7d707a3449245ba325055b54c42ff14
Igor Lysak <lysak.igor@gmail.com>
2022-04-06 17:30:28 +0300

    Dataflow example BigQuery dataset name is dns based  -

--- dataflow-example/main.tf
+++ dataflow-example/main.tf
@@ -1,30 +1,34 @@
 data "google_client_config" "current" {}
 
+locals {
+  dataset_name = replace(var.dataset_name, "/[[:punct:]]/", "_")
+}
+
 module "bigquery" {
   source  = "terraform-google-modules/bigquery/google"
   version = "~> 5.4"
 
-  dataset_id                 = var.dataset_name
-  dataset_name               = var.dataset_name
+  dataset_id                 = local.dataset_name
+  dataset_name               = local.dataset_name
   description                = var.description
   project_id                 = data.google_client_config.current.project
   location                   = var.location
   delete_contents_on_destroy = var.delete_contents_on_destroy
   tables = [
     {
-      table_id           = var.table_name,
-      schema             = file("table-schema.json"),
-      time_partitioning  = null,
-      range_partitioning = null,
-      clustering         = [],
-      expiration_time    = null,
+      table_id           = var.table_name
+      schema             = file("table-schema.json")
+      time_partitioning  = null
+      range_partitioning = null
+      clustering         = []
+      expiration_time    = null
       labels             = {}
     }
   ]
 }
 
 resource "google_bigquery_dataset_iam_member" "data_owner" {
-  dataset_id = var.dataset_name
+  dataset_id = local.dataset_name
   role       = "roles/bigquery.dataOwner"
   member     = "serviceAccount:${var.compute_service_account}"
 
@@ -34,7 +38,7 @@ resource "google_bigquery_dataset_iam_member" "data_owner" {
 }
 
 resource "google_bigquery_table_iam_member" "data_owner" {
-  dataset_id = var.dataset_name
+  dataset_id = local.dataset_name
   table_id   = var.table_name
   role       = "roles/bigquery.dataOwner"
   member     = "serviceAccount:${var.compute_service_account}"
