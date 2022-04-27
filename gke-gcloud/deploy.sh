3ac23d8d68c815698f9fdc769b115b8e9078b800
Antons Kranga <akranga@gmail.com>
2022-04-27 11:47:20 +0200

    Undeploy should not stop if 404 for service acocunt

--- gke-gcloud/deploy.sh
+++ gke-gcloud/deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-gcloud services enable container.googleapis.com
+if test -z "$HUB_KUBECONFIG"; then
+  HUB_KUBECONFIG="$(dotenv get "HUB_KUBECONFIG" --default "$KUBECONFIG")"
+fi
+export KUBECONFIG="$HUB_KUBECONFIG"
 
+gcloud services enable "container.googleapis.com"
 gcloud container clusters create "$NAME" --flags-file=gcloud-container-clusters-create.yaml
 
-echo 
+kubectl config delete-context "$HUB_DOMAIN_NAME" || echo "Moving on!"
+kubectl config rename-context "$(kubectl config current-context)" "$HUB_DOMAIN_NAME"
+
+echo
 echo "Outputs:"
 echo "gcp_url=https://console.cloud.google.com/kubernetes/clusters/details/$ZONE/$NAME/details?project=$PROJECT"
 echo
