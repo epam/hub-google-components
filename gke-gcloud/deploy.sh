03e6b93adafdd2cac2903fd8b8da2dd90b7a4cf6
Viktors Oginskis <viktors_oginskis@epam.com>
2022-04-28 13:13:42 +0300

    Revert "Undeploy should not stop if 404 for service acocunt"
    This reverts commit 3ac23d8d68c815698f9fdc769b115b8e9078b800.

--- gke-gcloud/deploy.sh
+++ gke-gcloud/deploy.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-if test -z "$HUB_KUBECONFIG"; then
-  HUB_KUBECONFIG="$(dotenv get "HUB_KUBECONFIG" --default "$KUBECONFIG")"
-fi
-export KUBECONFIG="$HUB_KUBECONFIG"
+gcloud services enable container.googleapis.com
 
-gcloud services enable "container.googleapis.com"
 gcloud container clusters create "$NAME" --flags-file=gcloud-container-clusters-create.yaml
 
-kubectl config delete-context "$HUB_DOMAIN_NAME" || echo "Moving on!"
-kubectl config rename-context "$(kubectl config current-context)" "$HUB_DOMAIN_NAME"
-
-echo
+echo 
 echo "Outputs:"
 echo "gcp_url=https://console.cloud.google.com/kubernetes/clusters/details/$ZONE/$NAME/details?project=$PROJECT"
 echo
--- gke-tf/deploy.sh
+++ gke-tf/deploy.sh
@@ -1,20 +1,18 @@
-#!/bin/bash -xe
+#!/bin/bash
 # shellcheck disable=SC2154
+
 
 terraform init -reconfigure -force-copy \
 		-backend-config="bucket=$STATE_BUCKET" \
 		-backend-config="prefix=$DOMAIN_NAME/$COMPONENT_NAME"
 
-TFPLAN=".terraform/$DOMAIN_NAME.plan"
+TFPLAN=.terraform/"$DOMAIN_NAME".plan
 
 terraform plan -out="$TFPLAN"
 terraform apply "$TFPLAN"
 
+
 gcloud container clusters get-credentials "$TF_VAR_name" --zone "$TF_VAR_location"
 
 # rename context to follow convention of the hub
-kubectl config rename-context "gke_${TF_VAR_project}_${TF_VAR_location}_${TF_VAR_name}" "${DOMAIN_NAME}"
-
-# expose kubernetes as a standard outputs, so it would
-# so it could provide 'kubernetes' for all other components
-kubeconfig -p tf-ouptuts -c "$DOMAIN_NAME"
+kubectl config rename-context gke_"$TF_VAR_project"_"$TF_VAR_location"_"$TF_VAR_name" "$DOMAIN_NAME"
