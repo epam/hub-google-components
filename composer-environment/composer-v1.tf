c4a1547cf82970345a9eb3a3cad9b9fca705906b
Igor Lysak <lysak.igor@gmail.com>
2022-04-29 18:36:43 +0300

    Prepare composer to work over shared vpc

--- composer-environment/composer-v1.tf
+++ composer-environment/composer-v1.tf
@@ -1,8 +1,10 @@
 # for additional parameters
 # see: https://github.com/terraform-google-modules/terraform-google-composer/tree/master/modules/create_environment_v1
 module "composer_v1" {
-  count  = var.composer_version == "v1" ? 1 : 0
-  source = "github.com/terraform-google-modules/terraform-google-composer//modules/create_environment_v1?ref=959db7d36dca43fa74866d79a91f1d5d1121fa16"
+  count = var.composer_version == "v1" ? 1 : 0
+
+  source  = "terraform-google-modules/composer/google//modules/create_environment_v1"
+  version = "~> 3.0"
 
   composer_env_name       = var.composer_env_name
   disk_size               = var.node_disk_size
@@ -10,20 +12,13 @@ module "composer_v1" {
   image_version           = local.airflow_image_version
   machine_type            = var.node_machine_type
   network                 = basename(data.google_compute_subnetwork.this.network)
-  network_project_id      = data.google_compute_subnetwork.this.project
+  network_project_id      = var.network_project_id
   node_count              = var.node_count
   project_id              = data.google_client_config.current.project
   pypi_packages           = local.pypi_packages
   python_version          = var.python_version
   region                  = data.google_client_config.current.region
   subnetwork              = data.google_compute_subnetwork.this.name
+  subnetwork_region       = data.google_compute_subnetwork.this.region
   zone                    = data.google_client_config.current.zone
-
-  # airflow_config_overrides  = {
-  #   core-dags_are_paused_at_creation = "True"
-  # }
-
-  # env_variables = {
-  #   FOO = "bar"
-  # }
 }
--- composer-environment/composer-v2.tf
+++ composer-environment/composer-v2.tf
@@ -1,9 +1,19 @@
+# for additional parameters
+# see: https://github.com/terraform-google-modules/terraform-google-composer/tree/master/modules/create_environment_v1
 module "composer_v2" {
-  count             = var.composer_version == "v2" ? 1 : 0
-  source            = "github.com/terraform-google-modules/terraform-google-composer//modules/create_environment_v2?ref=959db7d36dca43fa74866d79a91f1d5d1121fa16"
-  composer_env_name = var.composer_env_name
-  project_id        = data.google_client_config.current.project
-  region            = data.google_client_config.current.region
-  network           = basename(data.google_compute_subnetwork.this.network)
-  subnetwork        = data.google_compute_subnetwork.this.name
+  count = var.composer_version == "v2" ? 1 : 0
+
+  source  = "terraform-google-modules/composer/google//modules/create_environment_v2"
+  version = "~> 3.0"
+
+  composer_env_name       = var.composer_env_name
+  enable_private_endpoint = var.composer_endpoint == "private"
+  image_version           = local.airflow_image_version
+  network                 = basename(data.google_compute_subnetwork.this.network)
+  network_project_id      = var.network_project_id
+  project_id              = data.google_client_config.current.project
+  pypi_packages           = local.pypi_packages
+  region                  = data.google_client_config.current.region
+  subnetwork              = data.google_compute_subnetwork.this.name
+  subnetwork_region       = data.google_compute_subnetwork.this.region
 }
