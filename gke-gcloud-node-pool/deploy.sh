c9524fd07e9dd44e76e25c15fe0ff37f6c403b69
Antons Kranga <akranga@gmail.com>
2022-05-02 11:27:41 +0200

    This script has been merged with pre-undeploy

--- gke-gcloud-node-pool/deploy.sh
+++ gke-gcloud-node-pool/deploy.sh
@@ -4,7 +4,7 @@ echo "Checking presence of node pool $NAME for cluster $CLUSTER"
 if gcloud container node-pools describe "$NAME" --zone "$ZONE" --cluster "$CLUSTER" > /dev/null; then
   echo "Already exists"
 else
-  echo "Creating from file: gcloud-container-node-pools-create.yaml..."
+  color b "Creating new node pool for cluster $CLUSTER using: gcloud-container-node-pools-create.yaml..."
   gcloud container node-pools create "$NAME" --flags-file=gcloud-container-node-pools-create.yaml
 fi
 
--- gke-gcloud/deploy.sh
+++ gke-gcloud/deploy.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
-
-gcloud services enable container.googleapis.com
+#!/bin/bash -xe
 
 gcloud services enable "container.googleapis.com"
 
 echo "Checking presence of cluster: $NAME"
 if ! gcloud container clusters describe "$NAME" --zone "$ZONE" > /dev/null; then
+  color b "Creating new GKE cluster using: gcloud-container-clusters-create.yaml"
   gcloud container clusters create "$NAME" --flags-file=gcloud-container-clusters-create.yaml
-  kubectl config delete-context "$HUB_DOMAIN_NAME" > /dev/null 2>&1 || true
-  kubectl config rename-context "$(kubectl config current-context)" "$HUB_DOMAIN_NAME"
 else
-  echo "Cluster $NAME already exist, moving on!"
+  echo "Cluster $NAME already exist!"
 fi
+
+echo "Fetching kubeconfig..."
+gcloud container clusters get-credentials "$NAME" --zone "$ZONE"
+kubectl config delete-context "$DOMAIN_NAME" > /dev/null 2>&1 || true
+kubectl config rename-context "$(kubectl config current-context)" "$DOMAIN_NAME"
