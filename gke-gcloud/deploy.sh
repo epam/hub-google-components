60ffe6ca431cc0824ca440b2311f94086d96cbe2
Antons Kranga <akranga@gmail.com>
2022-05-21 19:38:48 +0300

    Making gke addons configurable

--- gke-gcloud/deploy.sh
+++ gke-gcloud/deploy.sh
@@ -2,10 +2,16 @@
 
 gcloud services enable "container.googleapis.com"
 
+if test -n "$CLUSTER_ADDONS"; then
+  # shellcheck disable=SC2089
+  GCLOUD_ARGS="$GCLOUD_ARGS --addons='$(echo "$CLUSTER_ADDONS"|tr ';' ' '|xargs|tr ' ' ',')'"
+fi
+
 echo "Checking presence of cluster: $NAME"
 if ! gcloud container clusters describe "$NAME" --zone "$ZONE" > /dev/null; then
   color b "Creating new GKE cluster using: gcloud-container-clusters-create.yaml"
-  gcloud container clusters create "$NAME" --flags-file=gcloud-container-clusters-create.yaml
+  # shellcheck disable=SC2086,SC2090
+  gcloud container clusters create "$NAME" --flags-file=gcloud-container-clusters-create.yaml $GCLOUD_ARGS
 else
   echo "Cluster $NAME already exist!"
 fi
