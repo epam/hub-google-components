db3647816725ed4a0de10cf5d6f59487c0d488c9
Antons Kranga <akranga@gmail.com>
2022-05-23 10:28:32 +0300

    gcloud command is quote sensitive

--- gke-gcloud/deploy.sh
+++ gke-gcloud/deploy.sh
@@ -4,13 +4,21 @@ gcloud services enable "container.googleapis.com"
 
 if test -n "$CLUSTER_ADDONS"; then
   # shellcheck disable=SC2089
-  GCLOUD_ARGS="$GCLOUD_ARGS --addons='$(echo "$CLUSTER_ADDONS"|tr ';' ' '|xargs|tr ' ' ',')'"
+  GCLOUD_ARGS="$GCLOUD_ARGS --addons=$(echo "$CLUSTER_ADDONS"|tr ';' ' '|xargs|tr ' ' ',')"
 fi
 
 echo "Checking presence of cluster: $NAME"
 if ! gcloud container clusters describe "$NAME" --zone "$ZONE" > /dev/null; then
   color b "Creating new GKE cluster using: gcloud-container-clusters-create.yaml"
   # shellcheck disable=SC2086,SC2090
+
+  cat << EOF
+
+Running gcloud:
+
+  gcloud container clusters create $NAME --flags-file=gcloud-container-clusters-create.yaml $GCLOUD_ARGS
+
+EOF
   gcloud container clusters create "$NAME" --flags-file=gcloud-container-clusters-create.yaml $GCLOUD_ARGS
 else
   echo "Cluster $NAME already exist!"
